8918821313bca6e879137ae5dc00809a67661dac
cbserra <789723+cbserra@users.noreply.github.com>
2022-07-15 04:21:48 -0700

    styled for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,39 @@
 module.exports = {
   content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'moderate-violet': 'hsl(263, 55%, 52%)',
+        'very-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+        'very-dark-blackish-blue': 'hsl(219, 29%, 14%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'light-gray': 'hsl(0, 0%, 81%)',
+        'light-grayish-blue': 'hsl(210, 46%, 95%)',
+      },
+
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+
+      backgroundImage: {
+        'quotes': 'url("./src/assets/bg-pattern-quotation.svg")',
+      },
+
+      fontSize: {
+        'xs': ['11px',{
+          lineHeight: '11px',
+        }],
+        'sm': ['13px',{
+          lineHeight: '13px',
+        }],
+        'md': ['13px',{
+          lineHeight: '18px',
+        }],
+        'lg': ['20px',{
+          lineHeight: '24px',
+        }],
+      },
+    },
   },
   plugins: [],
 }
